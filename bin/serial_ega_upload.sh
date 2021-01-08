4158d084ca932a91c01e3018fde6a292e4b135b9
Roman Valls Guimera <brainstorm@nopcode.org>
2021-01-08 17:31:28 +1100

    Serial encrypt&upload example script

---
+++ bin/serial_ega_upload.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+# Terribly inefficient submission script given a list of S3 files to submit.
+# Since EBI's EGA is known for dropping out connections when there is too much traffic, this could be useful in some cases (as slow, serial, upload method).
+#
+# NOTE: Modify $EGA_BOX and $PASSWORD to suit your needs
+
+for sample in `cat ega-files.txt`
+do
+        sample_name=`basename $sample`
+        time aws s3 cp $sample data/
+        time ./bin/submit.sh data/$sample $EGA_BOX $PASSWORD
+        rm -f data/$sample_name
+        sudo rm -rf data/enc/$sample_name
+done
